c74bb5c04fc59c1c2be0078a619809d4674b38b0
ekater1na <ekaterina_l@tut.by>
2022-12-14 00:10:32 +0300

    feat: update env vars

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,17 @@
+import 'dotenv/config';
 import mongoose from 'mongoose';
 
 import * as serverService from './services/server.service';
-import 'dotenv/config';
 
 const PORT = process.env.PORT || 4000;
-const DB_URI = process.env.DB_URI || 'mongodb+srv://admin:admin@cluster0.7mzgoou.mongodb.net/managerApp';
+const DB_URI = process.env.DB_URI || '';
 
 
 (async () => {
   try {
     await mongoose.connect(DB_URI);    
     serverService.server.listen(PORT, function () {
-      console.log('Server is waiting connection...');
-      console.log(DB_URI);
-
+      console.log(`Server is waiting connection on PORT ${PORT}`);
     })
   } catch (error) {
     console.log(error);
