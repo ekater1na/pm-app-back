c0b5279d2b18743cdf9dc686a2f153c6d30dd201
ekater1na <ekaterina_l@tut.by>
2022-12-13 23:08:33 +0300

    feat: update database url

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import * as serverService from './services/server.service';
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://admin:admin@cluster0.7mzgoou.mongodb.net/managerApp');
+    await mongoose.connect('mongodb+srv://admin:admin@cluster0.gkl5lmy.mongodb.net/managerApp');    
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
     })
